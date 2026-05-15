dd0a73c47c1c328ed0ee8b4a3327d5033e6c4dea
SamWang8891 <g348.8891@smashit.tw>
2026-05-16 02:48:48 +0800

    feat(widgets): LibraryShortcutWidget + bundle entry point

---
+++ swift/TigerDuckWidgets/TigerDuckWidgetsBundle.swift
@@ -0,0 +1,10 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct TigerDuckWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        LibraryShortcutWidget()
+        // Future tasks add: NextClassWidget(), TodayWidget(), WeekWidget(), AccessoryWidget()
+    }
+}
